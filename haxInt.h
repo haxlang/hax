c086d5ecf2e6fc4cf5421f59dddfc8743a21ae90
Kamil Rytarowski <n54@gmx.com>
2019-11-20 02:35:19 +0100

    Merge haxRegexp.h into haxInt.h

--- haxInt.h
+++ haxInt.h
@@ -32,9 +32,6 @@
 #ifndef _HAX
 #include "hax.h"
 #endif
-#ifndef _REGEXP
-#include "haxRegexp.h"
-#endif
 
 #include <ctype.h>
 #include <stdlib.h>
@@ -50,6 +47,31 @@
 #define HAX_ENOMEM 12
 #define HAX_EINVAL 22
 #define HAX_ERANGE 34
+
+/*
+ *----------------------------------------------------------------
+ * Definitions etc. for regexp(3) routines.
+ *
+ * Caveat:  this is V8 regexp(3) [actually, a reimplementation thereof],
+ * not the System V one.
+ *----------------------------------------------------------------
+ */
+
+#define NSUBEXP  10
+typedef struct regexp {
+	char *startp[NSUBEXP];
+	char *endp[NSUBEXP];
+	char regstart;		/* Internal use only. */
+	char reganch;		/* Internal use only. */
+	char *regmust;		/* Internal use only. */
+	int regmlen;		/* Internal use only. */
+	char program[1];	/* Unwarranted chumminess with compiler. */
+} regexp;
+
+extern regexp *RegComp (Hax_Interp *interp, char *exp);
+extern int RegExec (regexp *prog, char *string);
+extern void RegSub (regexp *prog, char *source, char *dest);
+extern void RegError (char *msg);
 
 /*
  *----------------------------------------------------------------
